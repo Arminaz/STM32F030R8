fb74d54f5b7a158067ae053fe5ceabb9a288751e
Arminaz Bakh <arminaz321@hotmail.ca>
2024-02-03 16:42:36 -0700

    Initialize Code

---
+++ src/builtin_blinker.c
@@ -0,0 +1,16 @@
+/**
+ * @file builtin_blinker.c
+ * @brief This is a simple Blinker that blinks the builtin LED for STM32F030R8 EVAL Board
+ * 
+ */
+#include <Arduino.h>
+
+const int ledPin = LED_BUILTIN;
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
