0dc7002359971fa537143c1443c923a38e2199bc
Arminaz Bakh <arminaz321@hotmail.ca>
2024-02-03 23:39:37 -0700

    Added Switch External LED and Updated all Code to CPP

---
+++ src/switch_extern_led.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file switch_external_led.c
+ * @brief Switch with External LED for STM32F030R8 EVAL Board.
+ * * Find video sample here: https://youtu.be/LdNNNbFCoaU
+ */
+
+#include <Arduino.h>
+
+const int btn = USER_BTN;
+const int led = LED_BUILTIN;
+const int e_led = D2;
+
+void flip()
+{
+	digitalWrite(e_led, !digitalRead(e_led));
+}
+
+void setup()
+{
+	pinMode(btn, INPUT);
+	pinMode(led, OUTPUT);
+	pinMode(e_led, OUTPUT);
+
+	attachInterrupt(digitalPinToInterrupt(btn), flip, FALLING);
+}
+
+void loop()
+{
+	digitalWrite(led, !digitalRead(led));
+	delay(1000);
+}
