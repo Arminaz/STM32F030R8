48f1056fa1783781c322a47c6d59960dccdf3316
Arminaz Bakh <arminaz321@hotmail.ca>
2024-02-03 22:10:17 -0700

    Added External Button Sample

---
+++ src/external_btn.c
@@ -0,0 +1,23 @@
+/**
+ * @file external_btn.c
+ * @brief External Button for STM32F030R8 EVAL Board.
+ * * Find video sample here: https://youtube.com/shorts/J1YUPy0i5zQ
+ */
+
+#include <Arduino.h>
+
+const int led = LED_BUILTIN;
+const int btn = D2;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(btn, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(btn) == LOW) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
