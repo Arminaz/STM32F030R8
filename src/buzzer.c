24eacce0a8ce9b518f6dead27eba561795cc3ce5
Arminaz Bakh <arminaz321@hotmail.ca>
2024-02-03 18:39:56 -0700

    Finished Buzzer Sample

---
+++ src/buzzer.c
@@ -0,0 +1,22 @@
+/**
+ * @file buzzer.c
+ * @brief Buzzer for STM32F030R8 EVAL Board.
+ * * Find video sample here: https://youtu.be/ghW0EezlxTU 
+ */
+
+#include <Arduino.h>
+
+const int buz = D2;
+
+void setup()
+{
+	pinMode(buz, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(buz, HIGH);
+	delay(1000);
+	digitalWrite(buz, LOW);
+	delay(1000);
+}
